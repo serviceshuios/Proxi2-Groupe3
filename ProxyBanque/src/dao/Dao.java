de2254ed2514b6ec36d46eac86bcaa5b0446d314
emartin6989 <emartin6989@gmail.com>
2017-04-11 13:54:33 +0200

    Dao et IDao avec ajouter conseiller

--- ProxyBanque/src/dao/Dao.java
+++ ProxyBanque/src/dao/Dao.java
@@ -3,6 +3,8 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -17,8 +19,8 @@ public class Dao implements IDao {
 		ConseillerClientele cc = null;
 		try {
 			Connection conn = DaoConnexion.getConnection();
-			PreparedStatement ps = conn
-					.prepareStatement("SELECT IdConseiller, Nom, Prenom, Adresse, CodePostal, Ville, Telephone, Email "
+			PreparedStatement ps = conn.prepareStatement(
+					"SELECT conseiller.IdConseiller, Nom, Prenom, Adresse, CodePostal, Ville, Telephone, Email "
 							+ "FROM conseiller, personne WHERE conseiller.IdPersonne = personne.IdPersonne "
 							+ "and  Login=? and MotDePasse=? ");
 			ps.setString(1, login);
@@ -54,7 +56,7 @@ public class Dao implements IDao {
 		try {
 			Connection conn = DaoConnexion.getConnection();
 			PreparedStatement ps = conn.prepareStatement(
-					"SELECT IdClient, TypeClient, Nom, Prenom, Adresse, CodePostal, Ville, Telephone, Email "
+					"SELECT client.IdClient, TypeClient, Nom, Prenom, Adresse, CodePostal, Ville, Telephone, Email "
 							+ "FROM client, personne WHERE client.IdPersonne = personne.IdPersonne "
 							+ "and client.IdConseiller=? ");
 			ps.setInt(1, IdConseiller);
@@ -84,5 +86,49 @@ public class Dao implements IDao {
 			DaoConnexion.closeConnection();
 		}
 		return cl;
+	}
+
+	@Override
+	public void ajouterConseiller(ConseillerClientele cc, String login, String mdp) {
+		try {
+			Connection conn = DaoConnexion.getConnection();
+			PreparedStatement ps = conn.prepareStatement("INSERT INTO personne(Nom, Prenom, Adresse, "
+					+ "CodePostal, Ville, Telephone, Email) " + "VALUES (?,?,?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, cc.getNom());
+			ps.setString(2, cc.getPrenom());
+			ps.setString(3, cc.getAdresse());
+			ps.setInt(4, cc.getCodePostal());
+			ps.setString(5, cc.getVille());
+			ps.setString(6, cc.getTelephone());
+			ps.setString(7, cc.getEmail());
+
+			int affectedRows = ps.executeUpdate();
+
+			if (affectedRows == 0) {
+				throw new SQLException("Creating user failed, no rows affected.");
+			}
+
+			ResultSet generatedKeys = ps.getGeneratedKeys();
+			if (generatedKeys.next()) {
+				int idpersonne = (int) generatedKeys.getLong(1);
+
+				PreparedStatement ps2 = conn.prepareStatement(
+						"INSERT INTO conseiller(Login, MotDePasse)" + "VALUES (?,?) WHERE conseiller.IdPersonne=?");
+				ps2.setString(1, login);
+				ps2.setString(2, mdp);
+				ps2.setInt(3, idpersonne);
+				ps2.executeUpdate();
+			} else {
+				throw new SQLException("Creating user failed, no ID obtained.");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			// code qui est executé quelles que soient les étapes précédentes
+			// 6-Fermer la connexion
+			DaoConnexion.closeConnection();
+		}
 	}
 }
--- ProxyBanque/src/dao/IDao.java
+++ ProxyBanque/src/dao/IDao.java
@@ -9,4 +9,5 @@ public interface IDao {
 
 	public ConseillerClientele authentificationConseiller(String login, String motDePasse);
 	public Collection<Client> listerClient(int IdConseiller);
+	public void ajouterConseiller(ConseillerClientele cc, String login, String mdp);
 }
