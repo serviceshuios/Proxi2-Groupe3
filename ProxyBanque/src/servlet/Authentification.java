6afefcfc3e85b5a90d2564d5e157f95adcc0fe5b
PierreJeanjacquot <pierre.jeanjacquot@gadz.org>
2017-04-11 12:00:31 +0200

    servlet authentification

---
+++ ProxyBanque/src/servlet/Authentification.java
@@ -0,0 +1,116 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import metier.ConseillerClientele;
+import service.AuthentificationService;
+import service.IAuthentificationService;
+
+/**
+ * Servlet implementation class Authentification
+ */
+@WebServlet("/Authentification")
+public class Authentification extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Authentification() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//récuperation des paramètres
+		String action = request.getParameter("action");
+		
+		String login = request.getParameter("login");
+		String mdp = request.getParameter("mdp");
+		
+		//
+		IAuthentificationService ica = new AuthentificationService();
+		
+		
+		//si la page à été appelée depuis authentifier
+		if (action.equals("authentifier")){
+		
+			if (login == null || mdp == null){
+				//TODO erreur saisie invalide
+				
+			}
+			else{
+				
+				ConseillerClientele conseiller = ica.authentificationConseiller(login, mdp);
+				
+				//si l'authentification échoue
+				if (conseiller == null){
+					
+					//ajout d'un message d'erreur et forward à la jsp erreur
+					request.setAttribute("msgErreur","Echec authentification Conseiller. Essayer à nouveau");
+					request.getRequestDispatcher("/erreur.jsp").forward(request, response);
+					
+				}
+				else {
+					
+					//récuperation de la session
+					HttpSession session = request.getSession();
+					
+					//ajout d'attributs à la session
+					//droits attribués
+					session.setAttribute("droits", "CONSEILLER");
+					//
+					session.setAttribute("idConseiller", conseiller.getIdConseiller());
+					session.setAttribute("nom", conseiller.getNom());
+					session.setAttribute("prenom", conseiller.getPrenom());
+					
+					//on forward à la jsp authentification
+					request.getRequestDispatcher("/authentification.jsp").forward(request, response);
+					
+				}
+				
+			}
+			
+			
+		}
+		//si la page à été appelée depuis deconnecter
+		else if (action.equals("deconnecter")){
+		
+			//recuperation de la session
+			HttpSession session = request.getSession();
+			//destruction de la session
+			session.invalidate();
+			//on forward à la jsp authentification
+			request.getRequestDispatcher("/authentification.jsp").forward(request, response);
+		
+		}
+		//si la page à été appelée d'un autre endroit on forward à la jsp authentification
+		
+		
+		else {
+			request.getRequestDispatcher("/authentification.jsp").forward(request, response);
+		}
+
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
