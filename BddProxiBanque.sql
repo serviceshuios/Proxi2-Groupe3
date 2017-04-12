3e0cb6ce84d59eeea7614772e5e48288fb1bd575
legalvincent056 <legalvincent056@gmail.com>
2017-04-12 16:35:32 +0200

    mcd + sql

--- BddProxiBanque.sql
+++ BddProxiBanque.sql
@@ -4,33 +4,20 @@
 
 
 #------------------------------------------------------------
-# Table: Personne
-#------------------------------------------------------------
-
-CREATE TABLE Personne(
-        Nom          Varchar (25) ,
-        Prenom       Varchar (25) ,
-        IdPersonne   int (11) Auto_increment  NOT NULL ,
-        Adresse      Varchar (50) ,
-        CodePostal   Varchar (5) ,
-        Ville        Varchar (25) ,
-        Telephone    Varchar (10) ,
-        Email        Varchar (50) ,
-        IdConseiller Int ,
-        IdClient     Int ,
-        PRIMARY KEY (IdPersonne )
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
 # Table: Conseiller
 #------------------------------------------------------------
 
 CREATE TABLE Conseiller(
         IdConseiller int (11) Auto_increment  NOT NULL ,
+        Nom          Varchar (25) ,
+        Prenom       Varchar (25) ,
+        Adresse      Varchar (50) ,
+        CodePostal   Int     (5)  ,
+        Ville        Varchar (25) ,
+        Telephone    Varchar (10) ,
+        Email        Varchar (50) ,
         Login        Varchar (50) ,
         MotDePasse   Varchar (50) ,
-        IdPersonne   Int ,
         PRIMARY KEY (IdConseiller )
 )ENGINE=InnoDB;
 
@@ -41,8 +28,14 @@ CREATE TABLE Conseiller(
 
 CREATE TABLE Client(
         IdClient     int (11) Auto_increment  NOT NULL ,
+        Nom          Varchar (25) ,
+        Prenom       Varchar (25) ,
+        Adresse      Varchar (50) ,
+        CodePostal   Int     (5)  ,
+        Ville        Varchar (25) ,
+        Telephone    Varchar (10) ,
+        Email        Varchar (50) ,
         TypeClient   Varchar (25) ,
-        IdPersonne   Int ,
         IdConseiller Int ,
         PRIMARY KEY (IdClient )
 )ENGINE=InnoDB;
@@ -53,12 +46,10 @@ CREATE TABLE Client(
 #------------------------------------------------------------
 
 CREATE TABLE Compte(
-        NumeroCompte    int (11) Auto_increment  NOT NULL ,
-        Solde           Float NOT NULL ,
-        DateOuverture   Date ,
-        IdClient        Int ,
-        IdCompteEpargne Int ,
-        IdCompteCourant Int ,
+        NumeroCompte  int (11) Auto_increment  NOT NULL ,
+        Solde         Float ,
+        DateOuverture Date ,
+        IdClient      Int ,
         PRIMARY KEY (NumeroCompte )
 )ENGINE=InnoDB;
 
@@ -69,9 +60,8 @@ CREATE TABLE Compte(
 
 CREATE TABLE CompteCourant(
         DecouvertAutorise Float ,
-        IdCompteCourant   int (11) Auto_increment  NOT NULL ,
         NumeroCompte      Int NOT NULL ,
-        PRIMARY KEY (IdCompteCourant )
+        PRIMARY KEY (NumeroCompte )
 )ENGINE=InnoDB;
 
 
@@ -81,18 +71,11 @@ CREATE TABLE CompteCourant(
 
 CREATE TABLE CompteEpargne(
         TauxRemuneration Float ,
-        IdCompteEpargne  int (11) Auto_increment  NOT NULL ,
         NumeroCompte     Int NOT NULL ,
-        PRIMARY KEY (IdCompteEpargne )
+        PRIMARY KEY (NumeroCompte )
 )ENGINE=InnoDB;
 
-ALTER TABLE Personne ADD CONSTRAINT FK_Personne_IdConseiller FOREIGN KEY (IdConseiller) REFERENCES Conseiller(IdConseiller);
-ALTER TABLE Personne ADD CONSTRAINT FK_Personne_IdClient FOREIGN KEY (IdClient) REFERENCES Client(IdClient);
-ALTER TABLE Conseiller ADD CONSTRAINT FK_Conseiller_IdPersonne FOREIGN KEY (IdPersonne) REFERENCES Personne(IdPersonne);
-ALTER TABLE Client ADD CONSTRAINT FK_Client_IdPersonne FOREIGN KEY (IdPersonne) REFERENCES Personne(IdPersonne);
 ALTER TABLE Client ADD CONSTRAINT FK_Client_IdConseiller FOREIGN KEY (IdConseiller) REFERENCES Conseiller(IdConseiller);
 ALTER TABLE Compte ADD CONSTRAINT FK_Compte_IdClient FOREIGN KEY (IdClient) REFERENCES Client(IdClient);
-ALTER TABLE Compte ADD CONSTRAINT FK_Compte_IdCompteEpargne FOREIGN KEY (IdCompteEpargne) REFERENCES CompteEpargne(IdCompteEpargne);
-ALTER TABLE Compte ADD CONSTRAINT FK_Compte_IdCompteCourant FOREIGN KEY (IdCompteCourant) REFERENCES CompteCourant(IdCompteCourant);
 ALTER TABLE CompteCourant ADD CONSTRAINT FK_CompteCourant_NumeroCompte FOREIGN KEY (NumeroCompte) REFERENCES Compte(NumeroCompte);
 ALTER TABLE CompteEpargne ADD CONSTRAINT FK_CompteEpargne_NumeroCompte FOREIGN KEY (NumeroCompte) REFERENCES Compte(NumeroCompte);
